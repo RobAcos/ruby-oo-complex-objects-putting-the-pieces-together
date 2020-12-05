8d97e592ca62cf2e67a52b360c07407460c208f2
xrojo <x.rojo@hotmail.com>
2020-12-05 15:35:17 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -53,17 +53,31 @@
 # end
 
 
+# class Shoe
+#     attr_accessor :color, :size, :material, :condition, :cobble
+#     attr_reader :brand 
+  
+#     def initialize(brand)
+#       @brand = brand
+#     end
+  
+#     def cobble
+#         @condition = "new"
+#         puts "Your shoe is as good as new!"
+#     end 
+  
+#   end 
+
 class Shoe
-    attr_accessor :color, :size, :material, :condition, :cobble
-    attr_reader :brand 
-  
-    def initialize(brand)
-      @brand = brand
-    end
-  
-    def cobble
-        @condition = "new"
-        puts "Your shoe is as good as new!"
-    end 
-  
-  end 
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
